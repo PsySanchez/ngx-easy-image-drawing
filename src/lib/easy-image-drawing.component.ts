a410d383b75c8c46edeb43759eb7f5fa20d0f2fb
Alex <alex.varonin@gmai.com>
2024-08-14 11:22:50 +0300

    change window listener to render

--- src/lib/easy-image-drawing.component.ts
+++ src/lib/easy-image-drawing.component.ts
@@ -8,6 +8,8 @@ import {
   EventEmitter,
   OnDestroy,
   OnChanges,
+  Renderer2,
+  SimpleChanges,
 } from "@angular/core";
 
 @Component({
@@ -20,7 +22,7 @@ import {
 export class EasyImageDrawing implements OnChanges, AfterViewInit, OnDestroy {
   @Input() width: number = 500;
   @Input() height: number = 500;
-  @Input() lineWidth = 7;
+  @Input() lineWidth: number = 7;
   @Input() lineColor = "#000000";
   @Input() src = "";
   @Input() saveButtonColor = "#4caf50";
@@ -33,18 +35,9 @@ export class EasyImageDrawing implements OnChanges, AfterViewInit, OnDestroy {
   context!: CanvasRenderingContext2D;
   private previousPosition: { x: number; y: number } = { x: 0, y: 0 };
   private activePath = false;
+  private eventListeners: (() => void)[] = [];
 
-  private _mousedown: any = null;
-  private _mousemove: any = null;
-  private _mouseup: any = null;
-
-  private _touchstart: any = null;
-  private _touchmove: any = null;
-  private _touchend: any = null;
-
-  ngOnChanges(): void {
-    this._setCanvas();
-  }
+  constructor(private renderer: Renderer2) {}
 
   ngAfterViewInit(): void {
     this._setCanvas();
@@ -52,6 +45,11 @@ export class EasyImageDrawing implements OnChanges, AfterViewInit, OnDestroy {
     this._setMouseEventsMobile();
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes["src"] && !changes["src"].firstChange) {
+      this.clear();
+    }
+  }
   public save(): void {
     const dataUrl = this.canvas.nativeElement.toDataURL("image/png");
     const blob = this._dataURItoBlob(dataUrl);
@@ -78,7 +76,7 @@ export class EasyImageDrawing implements OnChanges, AfterViewInit, OnDestroy {
     return new Blob([ab], { type: mimeString });
   }
 
-  private _drawLine(event: any): void {
+  private _drawLine(event: MouseEvent | Touch): void {
     const currentPosition = this.getPosition(event.clientX, event.clientY);
     this.context.moveTo(this.previousPosition.x, this.previousPosition.y);
     this.context.lineTo(currentPosition.x, currentPosition.y);
@@ -105,7 +103,12 @@ export class EasyImageDrawing implements OnChanges, AfterViewInit, OnDestroy {
     this.canvas.nativeElement.width = this.width;
     this.canvas.nativeElement.height = this.height;
 
-    this.context = this.canvas.nativeElement.getContext("2d")!;
+    const context = this.canvas.nativeElement.getContext("2d");
+    if (!context) {
+      console.error("Failed to get 2D context");
+      return;
+    }
+    this.context = context;
     this.context.lineWidth = this.lineWidth;
     this.context.strokeStyle = this.lineColor;
     this.context.lineCap = "round";
@@ -119,66 +122,67 @@ export class EasyImageDrawing implements OnChanges, AfterViewInit, OnDestroy {
   }
 
   private _setMouseEvents(): void {
-    this._mousedown = window.addEventListener(
-      "mousedown",
-      (event: MouseEvent) => {
-        this.activePath = true;
-        this.context.beginPath();
-        this.previousPosition = this.getPosition(event.clientX, event.clientY);
-      }
+    this.eventListeners.push(
+      this.renderer.listen("window", "mousedown", this._onMouseDown.bind(this)),
+      this.renderer.listen("window", "mousemove", this._onMouseMove.bind(this)),
+      this.renderer.listen("window", "mouseup", this._onMouseUp.bind(this))
     );
+  }
 
-    this._mousemove = window.addEventListener("mousemove", (event) => {
-      if (this.activePath) {
-        this._drawLine(event);
-      }
-    });
+  private _onMouseDown(event: MouseEvent): void {
+    this.activePath = true;
+    this.context.beginPath();
+    this.previousPosition = this.getPosition(event.clientX, event.clientY);
+  }
 
-    this._mouseup = window.addEventListener("mouseup", () => {
-      if (this.activePath) {
-        this.context.closePath();
-        this.activePath = false;
-      }
-    });
+  private _onMouseMove(event: MouseEvent): void {
+    if (this.activePath) {
+      this._drawLine(event);
+    }
+  }
+
+  private _onMouseUp(): void {
+    if (this.activePath) {
+      this.context.closePath();
+      this.activePath = false;
+    }
   }
 
   private _setMouseEventsMobile(): void {
-    this._touchstart = window.addEventListener(
-      "touchstart",
-      (event: TouchEvent) => {
-        this.activePath = true;
-        this.context.beginPath();
-        this.previousPosition = this.getPosition(
-          event.touches[0].clientX,
-          event.touches[0].clientY
-        );
-      }
+    this.eventListeners.push(
+      this.renderer.listen(
+        "window",
+        "touchstart",
+        this._onTouchStart.bind(this)
+      ),
+      this.renderer.listen("window", "touchmove", this._onTouchMove.bind(this)),
+      this.renderer.listen("window", "touchend", this._onTouchEnd.bind(this))
     );
-
-    this._touchmove = window.addEventListener("touchmove", (event) => {
-      if (this.activePath) {
-        this._drawLine(event.touches[0]);
-      }
-    });
-
-    this._touchend = window.addEventListener("touchend", () => {
-      if (this.activePath) {
-        this.context.closePath();
-        this.activePath = false;
-      }
-    });
   }
 
-  private _removeEventListeners(): void {
-    window.removeEventListener("mousedown", this._mousedown);
-    window.removeEventListener("mousemove", this._mousemove);
-    window.removeEventListener("mouseup", this._mouseup);
-    window.removeEventListener("touchstart", this._touchstart);
-    window.removeEventListener("touchmove", this._touchmove);
-    window.removeEventListener("touchend", this._touchend);
+  private _onTouchStart(event: TouchEvent): void {
+    this.activePath = true;
+    this.context.beginPath();
+    this.previousPosition = this.getPosition(
+      event.touches[0].clientX,
+      event.touches[0].clientY
+    );
+  }
+
+  private _onTouchMove(event: TouchEvent): void {
+    if (this.activePath) {
+      this._drawLine(event.touches[0]);
+    }
+  }
+
+  private _onTouchEnd(): void {
+    if (this.activePath) {
+      this.context.closePath();
+      this.activePath = false;
+    }
   }
 
   ngOnDestroy(): void {
-    this._removeEventListeners();
+    this.eventListeners.forEach((unlisten) => unlisten());
   }
 }
